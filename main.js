8221969d2767e2c5d51c052e95069a9f6fb2f988
unknown <kalnins.marks@gmail.com>
2019-05-02 17:55:53 +0300

    imgwithtiles

--- main.js
+++ main.js
@@ -2,19 +2,47 @@ let img;
 
 function preload(){
 
-img = loadImage('https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjT2trhwOvhAhWBw8QBHXGqA3gQjRx6BAgBEAU&url=https%3A%2F%2Fblog.hootsuite.com%2Finstagram-hashtags%2F&psig=AOvVaw0b8tysoc024oNhIlUE0OHE&ust=1556290689332416');
+img = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/June_odd-eyed-cat_cropped.jpg/1024px-June_odd-eyed-cat_cropped.jpg');
+img2 = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/June_odd-eyed-cat_cropped.jpg/1024px-June_odd-eyed-cat_cropped.jpg');
 }
 
 
 
 function setup(){
-createCanvas (1000,1000);
-image(img, 0, 0);
+createCanvas (2000,1700, WEBGL);
+img.loadPixels();
+img2.loadPixels();
 
 }
 
 function draw(){
-background(100);
+background(250);
+let col;
+image(img, 0, 0, 1000, 700);
 
+push();
+translate(-600,-400);
+image(img2,0,0,600,400);
+copy(img2,0,150,img2.width,1,0,150,img2.width,-img2.height-150);
+pop();
 
+for(var x = 0; x<1000; x+=10){
+for(var y = 0; y<700; y+=10){
+
+console.log(x);
+console.log(y);
+col = img.get(x,y);
+fill(col);
+//
+//box(40);
+//
+//ellipse(x,y,10,20);
+//
+push();
+translate(x,y);
+box(8);
+pop();
+
+}
+}
 }
